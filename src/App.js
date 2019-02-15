2e393482bc06dcadebac1317d0ee9c6066ce2c5e
kaery1000 <km4350@nyu.edu>
2019-02-14 22:57:00 -0500

    edited in app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Welcome to react. edited
+           Welcome to react.
           </p>
           <a
             className="App-link"
